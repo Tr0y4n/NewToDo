252b80cc3fd27b06dbaa756fe1e3fcfe743d8385
Tr0y4n <55826971+Tr0y4n@users.noreply.github.com>
2021-07-27 21:43:42 +0300

    base

--- src/components/canvas/Canvas.js
+++ src/components/canvas/Canvas.js
@@ -2,27 +2,35 @@ import { useState } from 'react';
 import './Canvas.css'
 import '../elems/Elems'
 import Elems from '../elems/Elems';
-
+import Button from '@material-ui/core/Button';
+import Input from '@material-ui/core/Input';
 
 function Canvas() {
 
-const [listToDo, setListToDo] = useState(['ds']);
+const [listToDo, setListToDo] = useState([]);
+const [textToDo, setTextToDo] = useState('');
 
 function handleClick() {
-let todo = prompt("Введите текст цели: ");
-setListToDo(listToDo.push(todo));
+//let todo = prompt("Введите текст цели: ");
+setListToDo([...listToDo, <Elems text = {textToDo} />]);
+setTextToDo("")
 }
 
+function handleChange(event) {
+  let text = event.target.value.slice(0,55);
+  setTextToDo(text);
+}
 
 return (
     <div className="Todos">
-      <ul>
+      
       {listToDo.map((ToDo) =>
-        <li key={listToDo[ToDo]}>{ToDo}</li>
+        <div className = "marg" key={listToDo[ToDo]}>{ToDo}</div>
       )}
-      </ul>
-        <h1>Привет, ниггеры!</h1>
-        <button className="butt" onClick={handleClick}>Добавить задание</button>
+      <div> 
+      <Input maxLe type="text" className="inp" value = {textToDo} onChange={handleChange}/>
+      <Button variant="contained" color="primary" className="butt marg" onClick={handleClick}>Добавить</Button>
+      </div>
     </div>
 );
 
--- src/components/elems/Elems.js
+++ src/components/elems/Elems.js
@@ -1,9 +1,25 @@
+import { useState } from 'react';
+import './Elems.css'
+import Checkbox from '@material-ui/core/Checkbox';
+
 function Elems(props) {
 
-    let arr = [];
+    const [checked, setChecked] = useState(false)
+    //const [textLine, setText]
+
+    function handleChange() {
+        setChecked(!checked);
+        
+    }
 
     return(
-        <div>{props.text}</div>
+        <div className="element" style = {{backgroundColor: checked ? 'lime' : 'rgb(229, 255, 0)'}}>
+        <Checkbox checked={checked} onChange={handleChange} value="checkedA" inputProps={{ 'aria-label': 'Checkbox A' }}/>
+            <span id="center" style = {{textDecoration: checked ? 'line-through' : 'none', color: checked ? 'grey' : 'black'}}>
+            {props.text}
+            </span>
+        <Checkbox className="op" value="checkedA" inputProps={{ 'aria-label': 'Checkbox A' }}/>
+        </div>
     )
 }
 
